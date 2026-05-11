b00fa471895c5958214e5c5ba582f0888d4a51a0
Areli Montelongo Prado <areli.montelongo0148@alumnos.udg.mx>
2026-05-10 19:35:42 -0600

    Configuración de ADC y OLED.c

--- ESP-Esclavo1/Esclavo1/main/esp1.c
+++ ESP-Esclavo1/Esclavo1/main/esp1.c
@@ -3,15 +3,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/adc.h"
-#include "esp_adc_cal.h"
 #include "driver_ledc.h"
 #include "driver/gpio.h"
+#include "ssd1306.h"
+#include "font8x8_basic.h"
 
-/* // Configuracion para ADC
+// Configuracion para ADC
 #define ADC_CHANNEL    ADC1_CHANNEL_4   // GPIO32
 #define ADC_ATTEN      ADC_ATTEN_DB_11  // Permite lectura hasta ~3.3V
 #define ADC_WIDTH      ADC_WIDTH_BIT_12 // Resolución de 12 bits
-*/
 
 // Configuracion para servo
 #define SERVO_GPIO      GPIO_NUM_23
@@ -21,8 +21,34 @@
 #define SERVO_CHANNEL   LEDC_CHANNEL_0
 #define SERVO_RES       LEDC_TIMER_16_BIT   // Resolución PWM
 
+// Configuracion para OLED
+#define CONFIG_SDA_GPIO		GPIO_NUM_21
+#define CONFIG_SCL_GPIO		GPIO_NUM_22
+#define CONFIG_RESET_GPIO	-1
+#define CONFIG_SSD1306_128x64
+#define tag "SSD1306"
+
 // Configuracion para boton de prueba
 #define BTN1 GPIO_NUM_12
+
+// Variables OLED
+int center=3;
+char lineChar[20];
+SSD1306_t dev;
+char lastLine[20] = "";
+
+// Variable para intensidad (deteccion de color)
+float intensidad = 0;
+
+// Configurar botón 
+void in_btn(void)
+{
+    gpio_reset_pin(BTN1);
+    gpio_set_direction(BTN1, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BTN1, GPIO_PULLUP_ONLY);
+}
+
+// SERVOMOTOR
 
 // Convierte ángulo a duty cycle
 uint32_t angle_to_duty(uint8_t angle)
@@ -33,19 +59,9 @@ uint32_t angle_to_duty(uint8_t angle)
     return min_duty + ((max_duty - min_duty) * angle) / 180;
 }
 
-void app_main(void)
+// Configurar temporizador PWM
+void pwm_timer_config(void)
 {
-    /*// Configurar ADC
-    adc1_config_width(ADC_WIDTH);
-    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN);
-    */
-
-    // Configurar botón
-    gpio_reset_pin(BTN1);
-    gpio_set_direction(BTN1, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(BTN1, GPIO_PULLUP_ONLY);
-
-    // Configurar temporizador PWM
     ledc_timer_config_t ledc_timer = {
         .speed_mode       = SERVO_MODE,
         .timer_num        = SERVO_TIMER,
@@ -55,8 +71,11 @@ void app_main(void)
     };
 
     ledc_timer_config(&ledc_timer);
+}
 
-    // Configurar canal PWM
+// Configurar canal PWM
+pwm_channel_config(void)
+{
     ledc_channel_config_t ledc_channel = {
         .channel    = SERVO_CHANNEL,
         .duty       = 0,
@@ -67,37 +86,105 @@ void app_main(void)
     };
 
     ledc_channel_config(&ledc_channel);
+}
 
-    int angle = 0;
-    int last_state = 1;
-
-    // Posición inicial
+// Mover servo al angulo indicado
+void servo_move(int angle)
+{
     ledc_set_duty(SERVO_MODE, SERVO_CHANNEL, angle_to_duty(angle));
     ledc_update_duty(SERVO_MODE, SERVO_CHANNEL);
+}
+
+// ADC
+
+// Configurar adc
+void in_adc(void)
+{
+    adc1_config_width(ADC_WIDTH);
+    adc1_config_channel_atten(ADC_CHANNEL, ADC_ATTEN);
+}
+
+// OLED
+void OLED(void)
+{
+	if(strcmp(lineChar, lastLine) != 0){
+   		ssd1306_clear_screen(&dev, false);
+    	ssd1306_display_text(&dev, center, lineChar, strlen(lineChar), false);
+    	strcpy(lastLine, lineChar);
+	}
+}
+
+// Deteccion de colores
+void leer_adc(void)
+{
+    int MAX = 1240; // Valor de prueba para máximo en blanco
+    int raw;
+    int mediciones;
+
+    vTaskDelay(pdMS_TO_TICKS(300));
+    // Captura de valores y promedio
+    OLED();
+    mediciones = 0;
+	for(int i=0; i<4; i++){
+		raw = adc1_get_raw(ADC_CHANNEL);
+		mediciones += raw;
+		float valor = ((float)raw/MAX)*100;
+		
+		sprintf(lineChar, "Valores: %.0f", valor);
+		OLED();		
+		vTaskDelay(pdMS_TO_TICKS(500));
+	}
+	intensidad = ((float)(mediciones/4.0)/MAX)*100;
     
+    // Color blanco o negro y movimiento correspondiente de servo
+	if(intensidad < 29){
+   		sprintf(lineChar, "Negro");
+   		servo_move(0);
+	}
+	else if(intensidad > 30){
+    	sprintf(lineChar, "Blanco");
+    	servo_move(60);
+	}
+	OLED();
+	
+    vTaskDelay(pdMS_TO_TICKS(500));	
+}
+
+
+void app_main(void)
+{
+    pwm_timer_config();
+    pwm_channel_config();
+
+    // Variable para flanco de bajada BTN1
+    int last_state_adc = 1;
+
+    Posicion inicial del servo
+    servo_move(0);
+
+    // Configuracion OLED
+    i2c_master_init(&dev, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
+    ssd1306_init(&dev, 128, 64);
+    vTaskDelay(pdMS_TO_TICKS(300));
+
+    // Inicializacion de ADC y botón
+    in_adc();
+    in_btn();
+
     // Bucle de lectura
     while (1) {
-        int current_state = gpio_get_level(BTN1);
+        // Boton prueba adc
+        int current_state_adc = gpio_get_level(BTN1);
 
-        // Detectar flanco de bajada (botón presionado)
-        if (last_state == 1 && current_state == 0)
+        // Detectar flanco de bajada
+        if (last_state_adc == 1 && current_state_adc == 0)
         {
-            // Cambiar entre 0° y 60°
-            if (angle == 0)
-                angle = 60;
-            else
-                angle = 0;+
+            leer_adc();
 
-            ledc_set_duty(SERVO_MODE, SERVO_CHANNEL, angle_to_duty(angle));
-            ledc_update_duty(SERVO_MODE, SERVO_CHANNEL);
-
-            // Pequeño debounce
+            // Debounce
             vTaskDelay(pdMS_TO_TICKS(200));
         }
-
-        last_state = current_state;
-
-        vTaskDelay(pdMS_TO_TICKS(10));
-        
+        last_state_adc = current_state_adc;
+        vTaskDelay(pdMS_TO_TICKS(10));        
     }
 }
